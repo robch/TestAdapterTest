21b43714f1dc7776651d42275a9ad0e436753cf3
Rob Chambers <robc@thinkman.com>
2022-01-19 18:33:54 -0800

    implement `RunTestCase`

--- YamlTestCaseRunner.cs
+++ YamlTestCaseRunner.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 using YamlDotNet.RepresentationModel;
@@ -38,9 +39,93 @@ namespace TestAdapterTest
 
         private static TestOutcome RunTestCase(TestCase test, string command, string script, string expect, string notExpect, string logExpect, string logNotExpect, out string stdOut, out string stdErr, out string errorMessage, out string additional, out string debugTrace, out TestOutcome outcome)
         {
-            // TODO: Actually run the test case here...
-            stdOut = stdErr = additional = debugTrace = errorMessage = null;
-            return outcome = TestOutcome.Skipped;
+            additional = $"START TIME: {DateTime.UtcNow}";
+            debugTrace = "";
+
+            Task<string> stdOutTask = null;
+            Task<string> stdErrTask = null;
+
+            try
+            {
+                script = WriteTextToTempFile(script);
+                expect = WriteTextToTempFile(expect);
+                notExpect = WriteTextToTempFile(notExpect);
+                logExpect = WriteTextToTempFile(logExpect);
+                logNotExpect = WriteTextToTempFile(logNotExpect);
+
+                var args = GetStartArgs(command, script, expect, notExpect, logExpect, logNotExpect);
+                var startInfo = new ProcessStartInfo("spx", args)
+                {
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    WorkingDirectory = Environment.CurrentDirectory
+                };
+
+                var process = Process.Start(startInfo);
+                stdOutTask = process.StandardOutput.ReadToEndAsync();
+                stdErrTask = process.StandardError.ReadToEndAsync();
+
+                process.WaitForExit();
+                outcome = process.ExitCode == 0
+                    ? TestOutcome.Passed
+                    : TestOutcome.Failed;
+
+                errorMessage = outcome == TestOutcome.Passed
+                    ? $"EXIT CODE: {process.ExitCode}"
+                    : null;
+                additional = ""
+                    + $"START TIME: {process.StartTime}"
+                    + $" STOP TIME: {process.ExitTime}"
+                    + $" EXIT CODE: {process.ExitCode}";
+            }
+            catch (Exception ex)
+            {
+                outcome = TestOutcome.Failed;
+                errorMessage = ex.Message;
+                debugTrace = ex.ToString();
+            }
+            finally
+            {
+                if (script != null) File.Delete(script);
+                if (expect != null) File.Delete(expect);
+                if (notExpect != null) File.Delete(notExpect);
+                if (logExpect != null) File.Delete(logExpect);
+                if (logNotExpect != null) File.Delete(logNotExpect);
+            }
+
+            stdOut = stdOutTask?.Result;
+            stdErr = stdErrTask?.Result;
+
+            return outcome;
+        }
+
+        private static string WriteTextToTempFile(string text)
+        {
+            if (!string.IsNullOrEmpty(text))
+            {
+                var tempFile = Path.GetTempFileName();
+                File.WriteAllText(tempFile, text);
+                return tempFile;
+            }
+            return null;
+        }
+
+        private static string GetStartArgs(string command, string script, string expect, string notExpect, string logExpect, string logNotExpect)
+        {
+            return !string.IsNullOrEmpty(command) || string.IsNullOrEmpty(script)
+                ? $"{command} {GetAtArgs(expect, notExpect, logExpect, logNotExpect)}"
+                : $"quiet run --script @{script} {GetAtArgs(expect, notExpect, logExpect, logNotExpect)}";
+        }
+
+        private static string GetAtArgs(string expect, string notExpect, string logExpect, string logNotExpect)
+        {
+            var atArgs = $"";
+            if (!string.IsNullOrEmpty(expect)) atArgs += $" --expect @{expect}";
+            if (!string.IsNullOrEmpty(notExpect)) atArgs += $" --not expect @{notExpect}";
+            if (!string.IsNullOrEmpty(logExpect)) atArgs += $" --log expect @{logExpect}";
+            if (!string.IsNullOrEmpty(logNotExpect)) atArgs += $" --log not expect @{logNotExpect}";
+            return atArgs.TrimStart();
         }
 
         private static TestOutcome SimulateTestCase(TestCase test, string simulate, string command, string script, string expect, string notExpect, string logExpect, string logNotExpect, out string stdOut, out string stdErr, out string errorMessage, out string additional, out string debugTrace, out TestOutcome outcome)
@@ -71,7 +156,6 @@ namespace TestAdapterTest
 
         private static TestOutcome OutcomeFromString(string simulate)
         {
-            Debugger.Launch();
             TestOutcome outcome = TestOutcome.None;
             switch (simulate?.ToLower())
             {
